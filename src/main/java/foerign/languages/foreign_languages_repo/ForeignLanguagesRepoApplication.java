bd3645e8e6bc85ea26a41833d9463e6cf36e9796
Sorin Gore <soringoredev@gmail.com>
2025-09-24 00:31:42 +0300

    Initial commit

---
+++ src/main/java/foerign/languages/foreign_languages_repo/ForeignLanguagesRepoApplication.java
@@ -0,0 +1,13 @@
+package foerign.languages.foreign_languages_repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForeignLanguagesRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForeignLanguagesRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/foerign/languages/foreign_languages_repo/ForeignLanguagesRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package foerign.languages.foreign_languages_repo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForeignLanguagesRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

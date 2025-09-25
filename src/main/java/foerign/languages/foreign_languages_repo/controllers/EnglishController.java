79cc9b9120f2522fd479f74b4775e01af3c34aea
Sorin Gore <soringoredev@gmail.com>
2025-09-25 12:43:08 +0300

    fix: add /api context-path and /v1 controller base

--- src/main/java/foerign/languages/foreign_languages_repo/controllers/EnglishController.java
+++ src/main/java/foerign/languages/foreign_languages_repo/controllers/EnglishController.java
@@ -1,9 +1,11 @@
 package foerign.languages.foreign_languages_repo.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/v1")
 public class EnglishController {
     @GetMapping("/en")
     public String englishGreeting() {
--- src/main/java/foerign/languages/foreign_languages_repo/controllers/FrancaisController.java
+++ src/main/java/foerign/languages/foreign_languages_repo/controllers/FrancaisController.java
@@ -2,11 +2,14 @@ package foerign.languages.foreign_languages_repo.controllers;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+// Asta trebuia sa faca Cristiana
+@RequestMapping("/v1")
 public class FrancaisController {
-    @GetMapping("v1/fr")
+    @GetMapping("/fr")
     public String francaisBonjour() {
         return "La France est le pays de l'amour.";
     }
